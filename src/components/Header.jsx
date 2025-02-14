b7cc8189ac7c0f7454a91b878298ff9c83e5362e
neutrino225 <fznkrmt22503@gmail.com>
2025-02-14 20:28:13 +0500

    updated header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,75 +4,11 @@ import { Link } from "react-router-dom";
 function Header() {
 	return (
 		<div>
-			{/* Topbar Start */}
-			<div className="container-fluid bg-dark px-5 d-none d-lg-block">
-				<div className="row gx-0">
-					<div className="col-lg-8 text-center text-lg-start mb-2 mb-lg-0">
-						<div
-							className="d-inline-flex align-items-center"
-							style={{ height: 45 }}>
-							<small className="me-3 text-light">
-								<i className="fa fa-map-marker-alt me-2" />
-								Tourify, Gulberg III, Lahore, Pakistan
-							</small>
-							<small className="me-3 text-light">
-								<i className="fa fa-phone-alt me-2" />
-								+92 300 1234567
-							</small>
-							<small className="text-light">
-								<i className="fa fa-envelope-open me-2" />
-								contact@tourify.pk
-							</small>
-						</div>
-					</div>
-
-					<div className="col-lg-4 text-center text-lg-end">
-						<div
-							className="d-inline-flex align-items-center"
-							style={{ height: 45 }}>
-							<a
-								className="btn btn-sm btn-outline-light btn-sm-square rounded-circle me-2"
-								href="/"
-								to="#">
-								<i className="fab fa-twitter fw-normal" />
-							</a>
-							<a
-								className="btn btn-sm btn-outline-light btn-sm-square rounded-circle me-2"
-								href="/"
-								to="#">
-								<i className="fab fa-facebook-f fw-normal" />
-							</a>
-							<a
-								className="btn btn-sm btn-outline-light btn-sm-square rounded-circle me-2"
-								href="/"
-								to="#">
-								<i className="fab fa-linkedin-in fw-normal" />
-							</a>
-							<a
-								className="btn btn-sm btn-outline-light btn-sm-square rounded-circle me-2"
-								href="/"
-								to="#">
-								<i className="fab fa-instagram fw-normal" />
-							</a>
-							<a
-								className="btn btn-sm btn-outline-light btn-sm-square rounded-circle"
-								href="/"
-								to="#">
-								<i className="fab fa-youtube fw-normal" />
-							</a>
-						</div>
-					</div>
-				</div>
-			</div>
-			{/* Topbar End */}
 			{/* Navbar & Hero Start */}
 			<div className="container-fluid position-relative p-0">
 				<nav className="navbar navbar-expand-lg navbar-light px-4 px-lg-5 py-3 py-lg-0">
 					<Link to="/" className="navbar-brand p-0">
-						<h1 className="text-primary m-0">
-							<i className="fa fa-map-marker-alt me-3" />
-							Tourify
-						</h1>
+						<h1 className="text-primary m-0">Tourify</h1>
 						{/* <img src="img/logo.png" alt="Logo"> */}
 					</Link>
 					<button
